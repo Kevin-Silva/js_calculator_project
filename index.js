15bcc92473005fab711cb51f6e6de9fdeddd8e6f
kevin.silva <kevin.t.silva@protonmail.com>
2023-12-24 09:08:03 -0300

    feature: some adjust to the calculator

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let buffer = '0';
 let screen = document.querySelector('.display')
+let runningTotal = '0'
 
 function buttonClick(value){
     if(isNaN(parseInt(value))){
@@ -18,8 +19,60 @@ function handleNumber(number){
     }
     console.log(buffer)
 }
-function handleSymbol(symbol){
+function handleMath(value){
+    if(buffer === '0'){
+        return;
+    }
+    const intBuffer = parseInt(buffer);
+    if(runningTotal === 0){
+        runningTotal = intBuffer;
+    }
+    else{
+        flushOperation(intBuffer);
+    }
     
+    previousOperator = value;
+    buffer = '0'
+}
+
+function flushOperation(intBuffer){
+    if(previousOperator === '+'){
+        runningTotal += intBuffer;
+    }
+    else if(previousOperator === '-'){
+        runningTotal -= intBuffer;
+    }
+    else if(previousOperator === 'x'){
+        runningTotal *= intBuffer;
+    }
+    else if(previousOperator === '/'){
+        runningTotal /= intBuffer;
+    }
+}
+
+function handleSymbol(symbol){
+    switch(symbol){
+        case 'C':
+            buffer =  '0'
+            break;
+        case '=':
+            if(previousOperator === null){
+                return;
+            }
+            flushOperation(parseInt(buffer));
+            buffer = runningTotal;
+            runningTotal = '0'
+        break;
+        case '+':
+
+        break;
+        case '-':
+        break;
+        case '/':
+        break;
+        case 'x':
+        break;
+    }
 }
 function init(){
     console.log('teste')
